aca67bc3de2efea7034af3ed9c958ce7787372b3
[Lawrencmagnat] <[chukwulawrencep@gmail.com]>
2023-02-27 06:29:15 +0100

    Lawrence Token

---
+++ lawrence-token/contracts/LawrenceToken.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^ 0.7.0 < 0.90;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract LawrenceToken is ERC20Capped, ERC20Burnable{
+    address payable public owner;
+uint256 public blockReward;
+
+    constructor(uint256 cap) ERC20("LawrenceToken", "LM") ERC20Capped(cap * (10 ** decimals())){
+        owner = msg.sender;
+        _mint(owner, 70000000 * (10 ** decimals()));
+        blockReward = reward * (10 ** decimals());
+    }
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint value) internal virtual override {
+        if(from != address(0) && to != block.coinbase != address(0)) {
+            _mintMinerReward();
+        }
+
+        super._beforeTokenTransfer(from, to, value);
+    }
+
+    function setBlockReward (uint256 reward) public onlyOwner{
+        blockReward = reward * (10 ** decimals());
+
+    }
+
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+}
