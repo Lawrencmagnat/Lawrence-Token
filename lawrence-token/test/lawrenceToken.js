ff5b82480296a4f6819bc9ceed2aa4e4cbaea3b8
[Lawrencmagnat] <[chukwulawrencep@gmail.com]>
2023-03-01 18:34:34 +0100

    The Competition

--- lawrence-token/test/lawrenceToken.js
+++ lawrence-token/test/lawrenceToken.js
@@ -34,22 +34,28 @@ describe("LawrenceToken contract", function() {
  expect(Number(hre.ethers.utils.formatEther(blockReward))).to.equal(tokenBlockReward);
  });
  });
+
  describe("Transactions", function () { 
  it("Should transfer tokens between accounts", async function () {
- // Transfer 50 tokens from owner to addr1 await oceanToken.transfer(addr1.address, 50); 
+ // Transfer 50 tokens from owner to addr1 
+ 
+ await lawrenceToken.transfer(addr1.address, 50); 
  const addr1Balance = await lawrenceToken.balanceOf(addr1.address); 
  expect(addr1Balance).to.equal(50); 
  // Transfer 50 tokens from addr1 to addr2 
  // We use .connect(signer) to send a transaction from another account
  await lawrenceToken.connect(addr1).transfer(addr2.address, 50); 
  const addr2Balance = await lawrenceToken.balanceOf(addr2.address); 
- expect(addr2Balance).to.equal(50); }); 
+ expect(addr2Balance).to.equal(50);
+
+}); 
+
  it("Should fail if sender doesn't have enough tokens", async function () { 
  const initialOwnerBalance = await lawrenceToken.balanceOf(owner.address);
  // Try to send 1 token from addr1 (0 tokens) to owner (1000000 tokens).
  // `require` will evaluate false and revert the transaction.
  await expect( 
- oceanToken.connect(addr1).transfer(owner.address, 1)
+ lawrenceToken.connect(addr1).transfer(owner.address, 1)
  ).to.be.revertedWith("ERC20: transfer amount exceeds balance"); 
  // Owner balance shouldn't have changed. 
  expect(await lawrenceToken.balanceOf(owner.address)).to.equal(
@@ -62,12 +68,12 @@ describe("LawrenceToken contract", function() {
  // Transfer 100 tokens from owner to addr1.
  await lawrenceToken.transfer(addr1.address, 100);
  // Transfer another 50 tokens from owner to addr2.
- await oceanToken.transfer(addr2.address, 50);
+ await lawrenceToken.transfer(addr2.address, 50);
  // Check balances. 
  const finalOwnerBalance = await lawrenceToken.balanceOf(owner.address);
  expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
  
- const addr1Balance = await oceanToken.balanceOf(addr1.address);
+ const addr1Balance = await lawrenceToken.balanceOf(addr1.address);
  expect(addr1Balance).to.equal(100); 
 
  const addr2Balance = await lawrenceToken.balanceOf(addr2.address);
